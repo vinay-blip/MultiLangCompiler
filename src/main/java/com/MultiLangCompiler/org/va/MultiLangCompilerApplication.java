34c2ec545beb19bc5655083f232990293cd929e0
ashishsingh9457 <ashishksingh9457@gmail.com>
2025-07-18 00:10:16 +0530

    Adding Project Files

---
+++ src/main/java/com/MultiLangCompiler/org/va/MultiLangCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.MultiLangCompiler.org.va;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLangCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiLangCompilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MultiLangCompiler/org/va/MultiLangCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MultiLangCompiler.org.va;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLangCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
