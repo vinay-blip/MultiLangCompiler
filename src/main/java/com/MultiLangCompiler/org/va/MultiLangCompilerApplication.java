9cb354cc79dc5dff210c83652f256a044eddcaf9
Vinay Rajput <vinayrajput@Vinays-MacBook-Air.local>
2025-07-22 23:13:05 +0530

    Add User Integration

--- src/main/java/com/MultiLangCompiler/org/va/MultiLangCompilerApplication.java
+++ src/main/java/com/MultiLangCompiler/org/va/MultiLangCompilerApplication.java
@@ -11,25 +11,6 @@ public class MultiLangCompilerApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context  = SpringApplication.run(MultiLangCompilerApplication.class, args);
-		UserRepository UR = context.getBean(UserRepository.class );
-		User user1 = new User();
-		user1.setGmail("vinayrajputtt4892@gmail.com");
-		user1.setName("Vinay Rajput");
-		user1.setNumber("9719399689");
-		user1.setPassword("vinayrajputtt4892@");
-		UR.save(user1);
-
-		User user2 = new User();
-		user2.setGmail("ashishkumarsingh4892@gmail.com");
-		user2.setName("Ashish Singh");
-		user2.setNumber("9719378372");
-		user2.setPassword("ashishkumarsingh4892@");
-		UR.save(user2);
-
-		Iterable<User> AllUsers = UR.findAll();
-		AllUsers.forEach(System.out::println);
-//		AllUsers.forEach(UR::delete);
-
 	}
 
 }
